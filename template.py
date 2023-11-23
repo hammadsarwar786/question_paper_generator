32fd222584292c676d55dda26359c0ef67506930
hammadsarwar786 <hammadarain326@gmail.com>
2023-11-23 10:24:07 +0500

    eror

---
+++ template.py
@@ -0,0 +1,316 @@
+# dummy_q = [
+#     'Explore the role of Network Trends New and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the relevance of Network Trends Online Collaboration in this context.',
+#     'Explore the role of Home Wireless Broadband and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Discuss the significance of Network Providing  and its relevance today.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Discuss the significance of Home Technology Trends  and its relevance today.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Discuss the significance of Home Powerline Networking  and its relevance today.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Elaborate on the role of Network Trends Online Collaboration in this field.',
+#     'Explore the role of Networking and draw lessons from their experiences.',
+#     'Discuss the significance of Network Trends Video  and its relevance today.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Home Technology Trends and draw lessons from their experiences.',
+#     'Explore the role of Network Scalable Networks and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'How does Network Topology Diagrams impact and what are the consequences?',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Area Network and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'How does Network Trends Online Collaboration impact and what are the consequences?',
+#     'How does Network Scalable Networks impact and what are the consequences?',
+#     'How does WANs Local Area Networks impact and what are the consequences?',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'How does Network Trends Online Collaboration influence our understanding?',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Network Fault Tolerance and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Network Providing and draw lessons from their experiences.',
+#     'How does Home Powerline Networking impact and what are the consequences?',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Components and draw lessons from their experiences.',
+#     'Discuss the significance of WANs Local Area Networks  and its relevance today.',
+#     'How does Network Trends Data Centers impact and what are the consequences?',
+#     'Discuss the significance of Network Trends New  and its relevance today.',
+#     'Discuss the significance of Networks Planning  and its relevance today.',
+#     'Explore the role of Networking and draw lessons from their experiences.',
+#     'Explore the role of Networks Planning and draw lessons from their experiences.',
+#     'Explore the role of Network Trends Data Centers and draw lessons from their experiences.',
+#     'Discuss the significance of WANs Wide Area Networks  and its relevance today.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of WANs Types and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of WANs Wide Area Networks and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Access Devices and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Discuss the significance of Networking Network  and its relevance today.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Discuss the significance of Network Topology Diagrams  and its relevance today.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Network Trends Video and draw lessons from their experiences.',
+#     'Explore the role of Network Clients and draw lessons from their experiences.',
+#     'Explore the role of Network Trends Cloud and draw lessons from their experiences.',
+#     'Discuss the significance of Network Trends Bring  and its relevance today.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Discuss the significance of Network Trends Data Centers  and its relevance today.',
+#     'Explore the role of Network End and draw lessons from their experiences.',
+#     'Explore the role of Network Peer and draw lessons from their experiences.',
+#     'Explore the role of Area Network and draw lessons from their experiences.',
+#     'Explore the role of Area Network and draw lessons from their experiences.',
+#     'Explore the role of Home Powerline Networking and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Network Topology Diagrams and draw lessons from their experiences.',
+#     'Discuss the significance of Network Trends Online Collaboration  and its relevance today.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Network Networks and draw lessons from their experiences.',
+#     'Explore the role of WANs Local Area Networks and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Discuss the significance of Converging Network  and its relevance today.',
+#     'Discuss the significance of Network Fault Tolerance  and its relevance today.',
+#     'Explore the role of Private Networks and draw lessons from their experiences.',
+#     'Explore the role of Network Trends Online Collaboration and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Network Trends Bring and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Network Packet and draw lessons from their experiences.',
+#     'Discuss the significance of Network Trends Cloud  and its relevance today.',
+#     'Discuss the significance of Home Wireless Broadband  and its relevance today.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Discuss the significance of NetworksPresentation_ID  and its relevance today.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Converging Network and draw lessons from their experiences.',
+#     'Explore the role of Networking Network and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Explore the role of Cisco Systems and draw lessons from their experiences.',
+#     'Discuss the significance of Network Scalable Networks  and its relevance today.',
+#     'Explore the role of NetworksPresentation_ID and draw lessons from their experiences.'
+# ]
+#
+# import PyPDF2
+#
+# def generate_question_paper(questions):
+#     pdf_file = open('templates/pape_temp.pdf', 'rb')
+#     pdf_reader = PyPDF2.PdfReader(pdf_file)
+#     pdf_writer = PyPDF2.PdfWriter()
+#
+#     # For each question, add a new page with the question details
+#     for i in range(0, len(questions), 3):
+#         pdf_writer.pages[0]
+#         page = pdf_writer.getPage(-1)
+#         content = []
+#
+#         for j in range(3):
+#             index = i + j
+#             if index < len(questions):
+#                 question = questions[index]
+#                 content.append(str(index + 1) + ". " + question)
+#
+#         # Update the page content with the question details
+#         page.setContents(content)
+#
+#     # Save the generated question paper
+#     with open('generated_question_paper.pdf', 'wb') as f:
+#         pdf_writer.write(f)
+#
+# generate_question_paper(dummy_q)
+
+# from PyPDF2 import PdfFileReader, PdfReader
+#
+# with open("templates/pape_temp.pdf", 'rb') as f:
+#     pdf = PdfReader(f)
+#     page = pdf.pages[0]
+#     text = page.extract_text()
+#
+# print(text)
+#
+#
+import os
+from fastapi.responses import JSONResponse
+from fastapi import FastAPI, WebSocket, APIRouter, HTTPException, Request, Depends
+from PyPDF2 import PdfWriter, PdfReader, Transformation
+import io
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.graphics import shapes
+import json
+import datetime
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from pathlib import Path
+
+
+class GenerateFromTemplate:
+    def __init__(self, template):
+        self.template_pdf = PdfReader(open(template, "rb"))
+        self.template_page = self.template_pdf.pages[0]
+
+        self.packet = io.BytesIO()
+        self.c = Canvas(self.packet, pagesize=(self.template_page.mediabox.width, self.template_page.mediabox.height))
+
+    def addText(self, text, point):
+        self.c.setFont("Helvetica", 10)
+        self.c.drawString(point[0], point[1], text)
+
+    def create_rect(self, q_number, questions):
+        x = 50
+        totalnumber = q_number
+
+        # rectangle
+        for i in range(totalnumber):
+            self.c.rect(height=72, width=520, x=x, y=(8-i) * 72,)
+
+        # data from api
+        parsed_data = questions
+        i = 0
+        for item in parsed_data:
+            if i == totalnumber:
+                break
+            num = item["number"]
+            if num == 1:
+                self.c.setFont("Helvetica", 10)
+                self.c.drawString(x+10, ((8 - (i - 0.5)) * 72), f"Q {i+1}")
+                self.c.drawString(x+30, ((8 - (i - 0.5)) * 72), item["question"][0])
+                self.c.drawString(x+490, ((8 - (i - 0.5)) * 72), "(10)")
+
+                i += 1
+            elif num == 2:
+                self.c.setFont("Helvetica", 10)
+                self.c.drawString(x+10, ((8 - (i - 0.5)) * 72), f"Q {i + 1}")
+                self.c.drawString(x+30, ((8 - (i - 0.3)) * 72), "(b)  " + item["question"][0])
+                self.c.drawString(x+30, ((8 - (i - 0.5)) * 72), "(a)  " + item["question"][1])
+                self.c.drawString(x + 493, ((8 - (i - 0.3)) * 72), "(5)")
+                self.c.drawString(x + 493, ((8 - (i - 0.5)) * 72), "(5)")
+
+                i += 1
+
+
+
+
+
+    def merge(self):
+        self.c.save()
+        self.packet.seek(0)
+        result_pdf = PdfReader(self.packet)
+        result = result_pdf.pages[0]
+
+        self.output = PdfWriter()
+
+        op = Transformation().rotate(0).translate(tx=0, ty=0)
+        result.add_transformation(op)
+        self.template_page.merge_page(result)
+        self.output.add_page(self.template_page)
+
+    def generate(self, dest):
+        outputStream = open(dest, "wb")
+        self.output.write(outputStream)
+        outputStream.close()
+
+    def generate_title(self, dep, smes, exam, time, subject, date, note):
+        x = 105
+        y = 9.85
+        x_bottom = 86
+        y_bottom = 9.54
+
+        dep = dep
+        semester = smes
+        exam_type = exam
+        time = time
+        subject = subject
+        date = date
+        teacher = "Ghulam Mustafa"
+        note = note
+
+        # Department
+        self.addText(dep, (x, y * 72))
+        # Semester
+        self.addText(semester, (x + 160, y * 72))
+        # Examination
+        self.addText(exam_type, (x + 290, y * 72))
+        # Time
+        self.addText(time, (x + 395, y * 72))
+
+        # Subject
+        self.addText(subject, (x_bottom, y_bottom * 72))
+        # Date
+        self.addText(date, (x_bottom + 160, y_bottom * 72))
+        # Examination
+        self.addText(teacher, (x_bottom + 285, y_bottom * 72))
+        # note
+        self.addText(note, (70, 9.2 * 72))
+
+
+
+
+router = APIRouter()
+@router.post("/paper")
+async def process_pdf(message: Request):
+    data = await message.json()
+    # parsed_data = json.loads(data)
+    # pdf path
+    gen = GenerateFromTemplate("templates/pape_temp.pdf")
+    # def generate_title(self, dep, smes, exam, time, subject, date, note):
+    # "title": title,
+    # "dep": dep,
+    # "class_name": class_name,
+    # "type": exam_type,
+    # "time": time,
+    # "date": paper_date,
+    # "note": note,
+    gen.generate_title(data["dep"], data["class_name"], data["type"], data["time"],
+                       data["title"], data["date"], data["note"])
+    gen.create_rect(data["qty"], data["pdf"])
+    gen.merge()
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    file_name = os.path.join("project\\question_generator\\assets\\papers", f"{timestamp}.pdf")
+    gen.generate(file_name)
+    return {"message": timestamp+".pdf", "number": data["qty"]}
+
+@router.get('/disp/{pdf_path:path}')
+async def display_pdf(pdf_path: str):
+    try:
+        # Build the full file path
+        full_path = Path(pdf_path)
+
+        # Check if the file exists
+        if full_path.is_file():
+            # Return the PDF file to the browser
+            return FileResponse(full_path)
+        else:
+            return {"message": "File not found"}, 404
+    except Exception as e:
+        return {"message": str(e)}, 500
+
+    # """
+    # Use as:
+    # gen = GenerateFromTemplate("template.pdf")
+    # gen.addText("Hello!",(100,200))
+    # gen.addText("PDF!",(100,300))
+    # gen.merge()
+    # gen.generate("Output.pdf")
+    # """
